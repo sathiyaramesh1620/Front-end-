304876b6ee25e7a23047a4226340dbb10611cf6e
sathiyaramesh <sathiyaramesh1620@gmail.com>
2022-10-03 22:42:04 +0530

    spread operator

---
+++ ES6/merge2.js
@@ -0,0 +1,13 @@
+let user={
+    id:101,
+    name:"sathiya",
+    email:"sathiyaanbuselvi@gmail,com"
+}
+let detail={
+    email:"sathiyaramesh@gmail.com",
+    address:"bangalore"
+}
+let user_details={
+    ...user,...detail
+}
+console.log(user_details)
---
+++ ES6/newarray.js
@@ -0,0 +1,9 @@
+
+
+//using spread operator
+let size=[1,2,3,4,5,6,7,8,9]
+let new_size=[...size]
+
+size[4]=35;
+console.log(size);
+console.log(new_size)
